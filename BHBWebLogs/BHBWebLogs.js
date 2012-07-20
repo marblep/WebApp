d1abe9309f344a66552ee60e329217f9eec30a47
unknown <maxiaobo@MaXiaoBo.SpicyNet.local>
2012-07-20 18:32:16 +0800

    select files

--- BHBWebLogs/BHBWebLogs.js
+++ BHBWebLogs/BHBWebLogs.js
@@ -3,49 +3,23 @@ window.onload = init;
 
 function init()
 {
-	window.webkitRequestFileSystem(window.PERSISTENT, 10*1024*1024, onInitFs,errorHandler);
-	alert(filesystem.root);
+	
 }
 
-function onInitFs(fs) {
-  console.log('Opened file system: ' + fs.name);
-  alert('Sucessful !!! Opened file system: ' + fs.name);
-  filesystem = fs;
-}
-
-function logerr(e) { console.log(e); }
-
-function readTextFile(name) {
-    // Get a File from a FileEntry from the root DirectoryEntry
-    var file = filesystem.root.getFile(name).file();
-    // Use the synchronous FileReader API to read it 
-    return new FileReaderSync().readAsText(file);
-}
-
-function errorHandler(e) {
-  var msg = '';
-
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'QUOTA_EXCEEDED_ERR';
-      break;
-    case FileError.NOT_FOUND_ERR:
-      msg = 'NOT_FOUND_ERR';
-      break;
-    case FileError.SECURITY_ERR:
-      msg = 'SECURITY_ERR';
-      break;
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'INVALID_MODIFICATION_ERR';
-      break;
-    case FileError.INVALID_STATE_ERR:
-      msg = 'INVALID_STATE_ERR';
-      break;
-    default:
-      msg = 'Unknown Error';
-      break;
-  };
-
-  console.log('Error: ' + msg);
-  alert('Error: ' + msg);
+function handlefiles(files)
+{
+	var reader = new FileReader();  // Create a FileReader object
+	reader.readAsText(files[0]);           // Read the file
+	reader.onload = function() 
+	{    // Define an event handler
+		var text = reader.result;   // This is the file contents
+		var out = document.getElementById("output");    // Find output element
+		out.innerHTML = text;                             // Clear it
+		console.log(text);
+		//out.appendChild(document.createTextNode(text)); // Display file contents
+	}
+	reader.onerror = function(e) 
+	{  // If anything goes wrong
+		console.log("Error", e);    // Just log it
+	};
 }
