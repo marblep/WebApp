afe886c4599bb00cfcac4d36a0b84ae4735daf3f
marblep <marblep@hotmail.com>
2013-06-28 17:36:18 +0800

    The first workable version!!!

--- PointsOutline/algorithm.js
+++ PointsOutline/algorithm.js
@@ -9,55 +9,92 @@ var Algorithm = {
         debugDrawEdgePoints(edgeList);
         
         var skylineSequence = [];
-        skylineSequence = skylineSequence.concat(leftToBottom(edgeList.left, edgeList.bottom));
-        console.log(skylineSequence.length + ", " + skylineSequence);
+        skylineSequence = skylineSequence.concat(collectFromSideToSide(edgeList.left, edgeList.bottom, _pointList, true, true));
+        skylineSequence = skylineSequence.concat(collectFromSideToSide(edgeList.bottom, edgeList.right, _pointList, true, false));
+        skylineSequence = skylineSequence.concat(collectFromSideToSide(edgeList.right, edgeList.top, _pointList, false, true));
+        skylineSequence = skylineSequence.concat(collectFromSideToSide(edgeList.top, edgeList.left, _pointList, false, false));
         
         return skylineSequence;
         
         
-        function leftToBottom(leftlist, bottomlist){
+        function collectFromSideToSide(fromList, toList, allPoints, bOutsideIsBelow, bxy){
             
             var skylinePoints = [];        
-            skylinePoints.push(pStart);
             
-            var candidates = _pointList.slice();
-            var pStart = leftlist[leftlist.length-1];
-            var pEnd = bottomlist[0];
+            var pStart = fromList[fromList.length-1];
+            var pEnd = toList[0];
             if(pStart == pEnd){
+                skylinePoints.push(pStart);
                 return skylinePoints;
             }
             
+            skylinePoints.push(pStart);
+            skylinePoints = skylinePoints.concat(getOutsidePoints(pStart, pEnd, allPoints, bOutsideIsBelow, bxy));
+            for(var i=0; i<toList.length; i++)
+                skylinePoints.push(toList[i]);
+            
+            return skylinePoints;
+        }
+        
+        
+        function getOutsidePoints(_pStart, _pEnd, _pointList, bOutsideIsBelow, bxy){
+            var resultList = [];
+            var candidates = _pointList.slice();
             while(true){
-                var line = new Line(pStart, pEnd);
-                candidates.splice(candidates.indexOf(pStart), 1);
-                candidates.splice(candidates.indexOf(pEnd), 1);
-                var smallCandidates = [];
+                
+                candidates = removeItem(candidates, [_pStart, _pEnd]);
+                var line = new Line(_pStart, _pEnd);
+                                
+                var outsidePoints = [];
                 for(var i=0; i<candidates.length; i++){
                     var p = candidates[i];
-                    if( line.getRelativePos(p) < 0 ){
-                        smallCandidates.push(p);
+                    if(bOutsideIsBelow){
+                        if( line.getRelativePos(p) < 0 ){
+                            outsidePoints.push(p);
+                        }
                     }
+                    else{
+                        if( line.getRelativePos(p) > 0 ){
+                            outsidePoints.push(p);
+                        }
+                    }
+                    
                 }
-                candidates = smallCandidates;
+                candidates = outsidePoints;
                 if(candidates.length == 0){
-                    skylinePoints.push(pEnd);
                     break;
                 }
                 
-                var point = candidates[0];
-                var minangle = Math.abs(point.x/point.y);
-                for(var i=1; i<candidates.length; i++){
-                    var p = candidates[i];
-                    var angle = Math.abs(p.x/p.y);
-                    if(angle < minangle){
-                        minangle = angle;
-                        point = p;
-                    }
-                }
-                skylinePoints.push(point);
-                pStart = point;
+                var point = getMinAnglePoint(_pStart, candidates, bxy);
+                resultList.push(point);
+                _pStart = point;
             }
-            return skylinePoints;
+            return resultList;
+        }
+        
+        function getMinAnglePoint(_pivot, _pList, bxy){
+            var minP = _pList[0];
+            var minangle = bxy ? Math.abs((minP.x-_pivot.x)/(minP.y-_pivot.y)) : Math.abs((minP.y-_pivot.y)/(minP.x-_pivot.x));
+            for(var i=1; i<_pList.length; i++){
+                var p = _pList[i];
+                var angle = bxy ? Math.abs((p.x-_pivot.x)/(p.y-_pivot.y)) : Math.abs((p.y-_pivot.y)/(p.x-_pivot.x));
+                if(angle < minangle){
+                    minangle = angle;
+                    minP = p;
+                }
+            }
+            return minP;
+        }
+        
+        function removeItem(fromList, items){
+            var list = fromList;
+            for(var i=0; i<items.length; i++){
+                var item = items[i];
+                var idx = list.indexOf(item);
+                if(idx >= 0)
+                    list.splice(idx, 1);
+            }
+            return list;
         }
         
         function getEdgePoints(_edgeList, _pointList){
@@ -117,7 +154,7 @@ var Algorithm = {
             var str = title + list.length + ";  ";
             for(var i=0; i<list.length; i++)
                 str += list[i].toString() + ", ";
-            console.log( str );
+            //console.log( str );
         }
         
         //logEdge("left", edgeList.left);
